8fb54f8a5a035ed4ac43776d8bcd480f7afb3d79
KazbekovAktenir <aktenirkazbekov@gmail.com>
2022-04-13 17:33:10 +0300

    hw2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, executor, Dispatcher, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from decouple import config
 import logging
+import random
 
 TOKEN = config("TOKEN")
 bot = Bot(TOKEN)
@@ -20,6 +21,13 @@ async def quiz_1(message: types.Message):
                         correct_option_id=0,
                         open_period=5
                         )
+
+@dp.message_handler(commands=['mem'])
+async def photo(message: types.Message):
+    photo = open("media/images (3).jpg", "rb")
+    await bot.send_photo(message.chat.id,
+                         photo=photo
+                         )
 
 
 @dp.message_handler(commands=['vopros'])
@@ -42,27 +50,70 @@ async def vopros_1(message: types.Message):
                         is_anonymous=False,
                         type='quiz',
                         correct_option_id=1,
-                        open_period=5
+                        open_period=5,
+                        reply_markup=knopka
                         )
 
-@dp.message_handler(commands = ['mem'])
-async def photo(message: types.Message):
-    photo = open("media/images (3).jpg", "rb")
-    await bot.send_photo(message.chat.id,
-        photo=photo
-                         )
+@dp.callback_query_handler(lambda func: func.data == "button_call_1")
+async def vopros_2(call: types.CallbackQuery):
+    knopka = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton(
+        "NEXT",
+        callback_data="button_call_1",
+    )
+    knopka.add(button_call_1)
 
-@dp.message_handler()
+    photo = open("media/photo_2022.jpg", "rb")
+    await bot.send_photo(call.message.chat.id,
+                         photo=photo)
+    question_2 = "Что напечатает такой код? "
+    answers = ['1', '2', 'True', 'False', 'Error']
+    await bot.send_poll(call.message.chat.id,
+                        question=question_2,
+                        options=answers,
+                        is_anonymous=False,
+                        type='quiz',
+                        correct_option_id=2,
+                        open_period=5,
+                        reply_markup=knopka
+                        )
+
+@dp.callback_query_handler(lambda func: func.data == "button_call_2")
+async def vopros_3(call_message: types.CallbackQuery):
+    knopka = InlineKeyboardMarkup()
+    button_call_2 = InlineKeyboardButton(
+        "NEXT",
+        callback_data="button_call_2",
+    )
+    knopka.add(button_call_2)
+    photo = open("media/vopros_3.jpg", "rb")
+    await bot.send_photo(call_message.message.chat.id,
+                         photo=photo)
+    question_3 = "Что выведет данная строка кода? "
+    answers = "True False Error".split()
+    await bot.send_poll(call_message.message.chat.id,
+                        question=question_3,
+                        options=answers,
+                        is_anonymous=False,
+                        type='quiz',
+                        correct_option_id=0,
+                        open_period=5,
+                        reply_markup=knopka
+                        )
+
+@dp.message_handler(content_types=["text"])
 async def echo_message(message: types.Message):
-    if message.text.isdigit():
-        chislo = int(message.text)
-        await message.answer(chislo ** 2)
-    else:
-        await message.answer(message.text)
+    # Send dice
+    if message.text.lower() == 'dice':
+        await bot.send_dice(message.chat.id, emoji="🎯")
+
+
+    #pin message
+    if message.text.startswith('pin'):
+        await bot.pin_chat_message(message.chat.id, message.message_id)
+
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=False)
-
-# ds
